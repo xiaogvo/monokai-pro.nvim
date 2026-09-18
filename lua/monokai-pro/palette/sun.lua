8716fca6dc813ffedd90a0f5a9defdfb384d606d
Xiao Guo <xiaogvo@icloud.com>
2026-09-19 04:26:45 +0800

    Add Monokai Pro Light (Filter Sun).

---
+++ lua/monokai-pro/palette/sun.lua
@@ -0,0 +1,18 @@
+---@type MonokaiPro.Palette
+return {
+  dark2 = "#d2c9c4",
+  dark1 = "#eee5de",
+  background = "#f8efe7",
+  text = "#2c232e",
+  accent1 = "#ce4770",
+  accent2 = "#d4572b",
+  accent3 = "#b16803",
+  accent4 = "#218871",
+  accent5 = "#2473b6",
+  accent6 = "#6851a2",
+  dimmed1 = "#72696d",
+  dimmed2 = "#92898a",
+  dimmed3 = "#a59c9c",
+  dimmed4 = "#beb5b3",
+  dimmed5 = "#d2c9c4",
+}
